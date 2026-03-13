d543b3e7f9d0da668819435059da05b536aa767d
modeEmmanuel <emmanuelmodebelu@gmail.com>
2026-03-13 06:27:12 +0100

    second commit-function creation and code clean-up

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,26 +5,33 @@ import (
 )
 
 func main() {
-	var revenue float64
-	var expense float64
-	var taxRate float64
+	// var revenue float64
+	// var expense float64
+	// var taxRate float64
 
-	fmt.Print("enter revenue:")
-	fmt.Scan(&revenue)
+	revenue := getUserInput("enter revenue:")
+	expenses := getUserInput("expenses")
+	taxRate := getUserInput("tax rate:")
+	ebt, profit, ratio := calFinancial(revenue, expenses, taxRate)
 
-	fmt.Print("expenses:")
-	fmt.Scan(&expense)
-
-	fmt.Print("tax rate:")
-	fmt.Scan(&taxRate)
-
-	ebt := revenue - expense
 	fmt.Println("earning before tax;:", ebt)
-
-	actualTaxRate := taxRate / 100
-	profit := ebt * (1 - actualTaxRate)
-	fmt.Println("Profit:", ebt)
-
-	ratio := ebt / profit
 	fmt.Println("ratio:", ratio)
+	fmt.Println("profit:", profit)
+}
+
+func getUserInput(prompt string) float64 {
+	var userInput float64
+	fmt.Print(prompt)
+	fmt.Scan(&userInput)
+
+	return userInput
+
+}
+
+func calFinancial(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	actualTaxRate := taxRate / 100
+	profit = ebt * (1 - actualTaxRate)
+	ratio = ebt / profit
+
 }
